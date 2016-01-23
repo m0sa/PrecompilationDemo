0e510f0c8bd8f168cb75009e101433771fd73449
m0sa <samo.prelog@gmail.com>
2016-01-23 23:58:25 +0100

    empty project

---
+++ PrecompilationDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+
+namespace PrecompilationDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sb = new StringBuilder();
+            Console.WriteLine("Who are you?");
+            var who = Console.ReadLine();
+            who = string.IsNullOrWhiteSpace(who) ? "stranger" : who;
+            sb.Append($"Hello {who}");
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
