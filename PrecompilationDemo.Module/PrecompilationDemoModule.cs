3efbc549d68e684ba3f70e084c091ce9ef4983b8
m0sa <samo.prelog@gmail.com>
2016-01-24 01:07:53 +0100

    compilation module implementation

--- PrecompilationDemo.Module/PrecompilationDemoModule.cs
+++ PrecompilationDemo.Module/PrecompilationDemoModule.cs
@@ -1,12 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using StackExchange.Precompilation;
 
 namespace PrecompilationDemo.Module
 {
-    public class PrecompilationDemoModule
+    public class PrecompilationDemoModule : ICompileModule
     {
+        public void BeforeCompile(BeforeCompileContext context)
+        {
+            foreach (var syntaxTree in context.Compilation.SyntaxTrees)
+            {
+                var model = context.Compilation.GetSemanticModel(syntaxTree);
+                var rewriter = new StringBuilderInterpolationOptimizer(model);
+                var rootNode = syntaxTree.GetRoot();
+                var rewritten = rewriter.Visit(rootNode);
+                if (rootNode != rewritten)
+                {
+                    context.Compilation = context.Compilation.ReplaceSyntaxTree(
+                        syntaxTree,
+                        syntaxTree.WithRootAndOptions(rewritten, syntaxTree.Options));
+                }
+            }
+        }
+
+        public void AfterCompile(AfterCompileContext context)
+        {
+        }
     }
 }
